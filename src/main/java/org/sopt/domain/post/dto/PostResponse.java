fbb99b58256096276a15ae4a6d46ad0b7b1a7616
5eoyng <szoyoung@naver.com>
2026-05-22 17:54:17 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/org/sopt/domain/post/dto/PostResponse.java
+++ src/main/java/org/sopt/domain/post/dto/PostResponse.java
@@ -1,7 +1,5 @@
 package org.sopt.domain.post.dto;
 
-import org.sopt.domain.post.entity.Post;
-import java.time.LocalDateTime;
 
 public record PostResponse(
         Long id,
@@ -11,19 +9,4 @@ public record PostResponse(
         String createdAt,
         Long likeCount
 ) {
-    // JPQL new 키워드용
-    public PostResponse(Long id, String title, String content, Long userId, LocalDateTime createdAt, long likeCount) {
-        this(id, title, content, userId, createdAt.toString(), likeCount);
-    }
-
-    public static PostResponse from(Post post, long likeCount) {
-        return new PostResponse(
-                post.getId(),
-                post.getTitle(),
-                post.getContent(),
-                post.getUser().getId(),
-                post.getCreatedAt().toString(),
-                likeCount
-        );
-    }
 }
